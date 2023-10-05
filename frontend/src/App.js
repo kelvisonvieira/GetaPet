8602acb14dba11cd6b4e2ed07b00cc9e2654a7e0
kelvisonvieira <kelvison.vieira@live.com>
2023-10-05 12:20:29 -0300

    layouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
 import { BrowserRouter as Router, Route} from 'react-router-dom'
+
+//componentes
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
+
 //import pages
 import Login from './components/pages/Auth/Login'
 import Register  from './components/pages/Auth/Register'
 import Home from './components/pages/Home';
+
+
 function App() {
   return (
     <Router>
+      <Navbar/>
        <Route path="/login">
-         <Login> </Login>
+         <Login/> 
        </Route>
        <Route path="/register">
-        <Register></Register>
+        <Register/>
        </Route>
        <Route path="/">
-        <Home></Home>
+        <Home/>
        </Route>
-       
+       <Footer/>
     </Router>
   );
 }
